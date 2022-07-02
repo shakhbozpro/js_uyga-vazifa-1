5b91ec91b91f3d0ed67e2b0642125ccb56f0de7e
shakhbozpro <shakhbozmamatkulov111@gmail.com>
2022-07-02 11:31:36 +0500

    Made alert questions with js promp and alert

---
+++ main.js
@@ -0,0 +1,51 @@
+var firstName = prompt("Ismingizni kiriting");
+var lastName = prompt("Familiyangizni kiriting");
+var userAge = prompt("Yoshingizni kiriting");
+var userAddress = prompt("Yashash joyingizni kiriting");
+var userHobby = prompt("Hobby ni kiriting");
+var userSchool = prompt("Talim olgan joyingizni kiriting");
+var userProfession = prompt("Kasbingizni kiriting");
+var userInterest = prompt("Qaysi sport turini yoqtirasiz?");
+var userFamily = prompt("Oilada necha kishisiz?");
+var userTeacher = prompt("Ustozingiz kim?");
+var userTel = prompt("Tel raqamingiz");
+var userEmail = prompt("Eamilingizni kiriting");
+var ourQuestion = prompt("Telegram kanalimizga obuna bo'lganmisiz?");
+var userStudy = prompt("Online kursimizni bitirganmisiz?");
+var lastQuestion = prompt("Hamma savolimizga anniq javob berdingizmi?");
+
+var info = alert(`
+Siznig ismingiz: ${firstName}
+Siznig familiyangiz: ${lastName}
+Siznig yoshingiz: ${userAge}
+Siznig manzilingiz: ${userAddress}
+Siznig hobbyngiz: ${userHobby}
+Siznig talim olgan joyingiz: ${userSchool}
+Siznig kasbingiz: ${userProfession}
+Siznig yoqtirgan sport turingiz: ${userInterest}
+Siznig oilangiz soni: ${userFamily}
+Siznig ustozingiz: ${userTeacher}
+Siznig tel raqamingiz: ${userTel}
+Siznig emailingiz: ${userEmail}
+Siznig javobingiz: ${ourQuestion}
+Siznig online kurs haqida javobingiz: ${userStudy}
+Siznig ohirgi javobingiz: ${lastQuestion}
+`);
+
+console.log(`
+Siznig ismingiz: ${firstName}
+Siznig familiyangiz: ${lastName}
+Siznig yoshingiz: ${userAge}
+Siznig manzilingiz: ${userAddress}
+Siznig hobbyngiz: ${userHobby}
+Siznig talim olgan joyingiz: ${userSchool}
+Siznig kasbingiz: ${userProfession}
+Siznig yoqtirgan sport turingiz: ${userInterest}
+Siznig oilangiz soni: ${userFamily}
+Siznig ustozingiz: ${userTeacher}
+Siznig tel raqamingiz: ${userTel}
+Siznig emailingiz: ${userEmail}
+Siznig javobingiz: ${ourQuestion}
+Siznig online kurs haqida javobingiz: ${userStudy}
+Siznig ohirgi javobingiz: ${lastQuestion}
+`);
